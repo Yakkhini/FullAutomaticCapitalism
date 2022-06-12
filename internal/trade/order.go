9e518943a5e48f4a7c04e5d44f243ae4819fc6dd
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-06-12 19:41:42 +0800

    Feat: Trade priority operation.

---
+++ internal/trade/order.go
@@ -0,0 +1,30 @@
+// order
+package trade
+
+func Postorder() {
+
+}
+
+func Posttest() {
+
+}
+
+func Postbatchorder() {
+
+}
+
+func Delorder() {
+
+}
+
+func Delallorder() {
+
+}
+
+func Postlever() {
+
+}
+
+func Postmagintype() {
+
+}
